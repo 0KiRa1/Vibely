3d0d2536638775b35290d64facd6ae68e6ca08f0
0KiRa1 <aarjavjain035@gmail.com>
2026-08-14 14:21:59 +0530

    change

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,40 +1,33 @@
 import { Inngest } from "inngest";
-import User from "../models/User.js";
+import User from "../models/User.js"; // Make sure to import your User model!
 
-// Create Inngest Client
-export const inngest = new Inngest({
-  id: "vibely-app",
-});
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "vibely-app" });
 
-// Sync New User from Clerk
+// Inngest function to save user data to the database
 const syncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
-    trigger: {
-      event: "clerk/user.created",
-    },
+    // Move event inside the first object!
+    triggers: [{ event: "clerk/user.created" }], 
   },
   async ({ event }) => {
-    const {
-      id,
-      first_name,
-      last_name,
-      email_addresses,
-      image_url,
-    } = event.data;
+    // Destructure event from argument object
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    
+    // Note: Clerk payload property name is email_addresses (two 's's, not 'addressess')
+    const primaryEmail = email_addresses?.[0]?.email_address || "";
+    let username = primaryEmail.split("@")[0];
 
-    let username = email_addresses[0].email_address.split("@")[0];
-
-    // Check username availability
-    const existingUser = await User.findOne({ username });
-
-    if (existingUser) {
-      username = `${username}${Math.floor(Math.random() * 1000)}`;
+    // Check availability of username
+    const user = await User.findOne({ username });
+    if (user) {
+      username = username + Math.floor(Math.random() * 1000);
     }
 
     const userData = {
       _id: id,
-      email: email_addresses[0].email_address,
+      email: primaryEmail,
       full_name: `${first_name || ""} ${last_name || ""}`.trim(),
       username,
       profile_picture: image_url,
@@ -44,49 +37,39 @@ const syncUserCreation = inngest.createFunction(
   }
 );
 
-// Sync Updated User from Clerk
+// Inngest function to update user data in database
 const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
-    trigger: {
-      event: "clerk/user.updated",
-    },
+    triggers: [{ event: "clerk/user.updated" }],
   },
   async ({ event }) => {
-    const {
-      id,
-      first_name,
-      last_name,
-      email_addresses,
-      image_url,
-    } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const primaryEmail = email_addresses?.[0]?.email_address || "";
 
-    const updatedUserData = {
-      email: email_addresses[0].email_address,
+    const updateUserData = {
+      email: primaryEmail,
       full_name: `${first_name || ""} ${last_name || ""}`.trim(),
       profile_picture: image_url,
     };
 
-    await User.findByIdAndUpdate(id, updatedUserData);
+    await User.findByIdAndUpdate(id, updateUserData);
   }
 );
 
-// Sync Deleted User from Clerk
+// Inngest function to delete user data in database
 const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-from-clerk",
-    trigger: {
-      event: "clerk/user.deleted",
-    },
+    triggers: [{ event: "clerk/user.deleted" }],
   },
   async ({ event }) => {
     const { id } = event.data;
-
     await User.findByIdAndDelete(id);
   }
 );
 
-// Export Functions
+// Export Inngest functions
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
