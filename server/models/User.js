8f63701cfab8cff36c66c83bdffcade9a5577e79
0KiRa1 <aarjavjain035@gmail.com>
2026-08-15 12:24:23 +0530

    update

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    _id: {type: String, required: true},
-    email: {type: String, required: true},
-    full_name: {type: String, required: true},
-    username: {type: String, required: true},
-    bio: {type: String, required: 'Hey there! I am using Vibely. Join me on my journey to build a better world. 🌎'},
-    profile_picture: {type: String, required: ''},
-    cover_photo: {type: String, required: ''},
-    location: {type: String, required: ''},
-    followers: [{type: String, ref: 'User'}],
-    following: [{type: String, ref: 'User'}],
-    connections: [{type: String, ref: 'User'}],
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    full_name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    bio: {
+      type: String,
+      default: "Hey there! I am using Vibely. Join me on my journey to build a better world. 🌎",
+    },
+    profile_picture: { type: String, default: "" },
+    cover_photo: { type: String, default: "" },
+    location: { type: String, default: "" },
+    followers: [{ type: String, ref: "User" }],
+    following: [{ type: String, ref: "User" }],
+    connections: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false }
+);
 
-}, {timestamps: true, minimize: false});
-
-const User = mongoose.model('User', UserSchema);
+// Prevent re-compiling model in serverless hot-reloads
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
